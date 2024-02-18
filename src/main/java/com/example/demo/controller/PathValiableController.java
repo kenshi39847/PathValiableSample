1766c0b1a946ff01cfd00b3ac3d0cfaec4b08e5f
kenshi39847 <kenshi.t.blue@gmail.com>
2024-02-18 14:07:19 +0900

    コントローラへの追記

--- src/main/java/com/example/demo/controller/PathValiableController.java
+++ src/main/java/com/example/demo/controller/PathValiableController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PathValiableController {
@@ -10,6 +11,26 @@ public class PathValiableController {
 	public String showView() {
 		//戻り値はビュー名を返す
 		return "show";
+	}
+	
+	//リンク処理
+	@GetMapping("/function/{no}")
+	public String selectFunction(@PathVariable Integer no) {
+		//ビュー名の初期化
+		String view = null;
+		switch(no) {
+		case 1:
+			view = "pathvaliable/function1";
+			break;
+		case 2:
+			view = "pathvaliable/function2";
+			break;
+		case 3:
+			view = "pathvaliable/function3";
+			break;
+		}
+		//戻り値はビュー名を返す
+		return view;
 	}
 
 }
